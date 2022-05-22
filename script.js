e13129b7545581ba73305df59825f23aec6d4dd0
Iris <iristryba@gmail.com>
2022-05-22 20:47:38 +0200

    changed done toggle behavior
    added toggling checkboxes, added icons for checkboxes and for trashcan, changed list item elements (new class names and tags)
    js: exchanged 'var' for 'let', exchanged keyCode for key

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var addButton = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+let addButton = document.getElementById("enter");
+let input = document.getElementById("userinput");
+let ul = document.querySelector("ul");
 
 window.onload = function() {
 	input.focus();
+	input.value = "";
 }
 
 function inputLength() {
@@ -11,13 +12,28 @@ function inputLength() {
 }
 
 function createListElement() {
-	var li = document.createElement("li");
-	var deleteButton = document.createElement("button");
-	deleteButton.appendChild(document.createTextNode("Delete"));
-	li.appendChild(document.createTextNode(input.value));
+	let li = document.createElement("li");
+
+	let spanCheckbox = document.createElement("span");
+	spanCheckbox.appendChild(document.createTextNode("check_box_outline_blank"));
+	spanCheckbox.classList.add("checkbox");
+	spanCheckbox.classList.add("material-symbols-outlined");
+		
+	let spanText = document.createElement("span");
+	spanText.appendChild(document.createTextNode(input.value));
+	spanText.classList.add("list-text");
+	
+	let deleteButton = document.createElement("button");
+	deleteButton.appendChild(document.createTextNode("delete"));
+	deleteButton.classList.add("delete-button");
+	deleteButton.classList.add("material-symbols-outlined");
+
+	li.appendChild(spanCheckbox);
+	li.appendChild(spanText);
 	li.appendChild(deleteButton);
 	ul.appendChild(li);
 
+	// reset input field
 	input.value = "";
 	input.focus();
 }
@@ -29,19 +45,29 @@ function addListAfterClick() {
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
+	if (inputLength() > 0 && event.key === "Enter") {
 		createListElement();
 	}
 }
 
 function toggleDone(listItem) {
-	if (listItem.target.tagName === "SPAN") {
-		listItem.target.classList.toggle("done");
+	if (listItem.target.classList.contains("checkbox")) {
+		listItem.target.firstChild.nodeValue = toggleCheckmark(listItem.target.firstChild.nodeValue);
+		listItem.target.nextElementSibling.classList.toggle("done");
+
+	}
+}
+
+function toggleCheckmark(checkmark) {
+	if (checkmark === "check_box_outline_blank") {
+		return "select_check_box";
+	} else {
+		return "check_box_outline_blank";
 	}
 }
 
 function deleteItem(listItem) {
-	if (listItem.target.tagName === "BUTTON") {
+	if (listItem.target.classList.contains("delete-button")) {
 		listItem.target.parentElement.remove();
 	}
 }
